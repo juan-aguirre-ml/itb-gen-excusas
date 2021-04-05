bd92d36208fd8ca6f46e04623d19cd3e829ffc7e
juan-aguirre-ml <aguirre.juan@mercadolibre.com>
2021-04-05 12:49:59 -0300

    initial

---
+++ src/main/java/com/itb/excusas/ExcusasApplication.java
@@ -0,0 +1,13 @@
+package com.itb.excusas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcusasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcusasApplication.class, args);
+    }
+
+}
